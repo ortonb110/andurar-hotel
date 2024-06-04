b559aaa59a6f5fefe2e0182761bc95926b37ab3e
Orton Bright <ortonb110@gmail.com>
2024-06-04 19:17:13 +0000

    Minor changes

--- src/Components/HomeBody.jsx
+++ src/Components/HomeBody.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import Card from "./Card"
 
 const HomeBody = () => {
   return (
-    <main>HomeBody</main>
+    <main>
+      <Card />
+      <Card />
+      <Card />
+    </main>
   )
 }
 
